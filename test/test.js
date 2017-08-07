9d4f04f5183c8c64a9689263e81736ef4320739c
Tomas Ruiz <tomastrg22@gmail.com>
2017-08-07 20:32:16 +0200

    refactor function callback to arrow syntax

--- test/test.js
+++ test/test.js
@@ -17,11 +17,12 @@ const apiKey = process.env.API_KEY;
 //   });
 // });
 
-describe('function getLanguageList', function() {
-  it('should return an object with dirs key that contain a non empty array', async function() {    
+describe('function getLanguageList', () => {
+  it('should return an object with dirs key that contain a non empty array', async () => {    
     const result = await apiWrapper.getLanguageList(process.env.API_KEY_YANDEX_TRANSLATE);
     expect(result).to.be.a('object');
     expect(result).to.include.all.keys('dirs')
     expect(result.dirs).to.be.an('array').that.is.not.empty;
   });
-});
+
+  
