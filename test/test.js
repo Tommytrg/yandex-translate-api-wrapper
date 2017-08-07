71890e775791b403b271ebac2fbfc19ebb2a331b
Tomas Ruiz <tomastrg22@gmail.com>
2017-08-07 21:56:36 +0200

    remove comments

--- test/test.js
+++ test/test.js
@@ -7,16 +7,6 @@ const expect =  require('chai').expect;
 const apiWrapper = require('../lib/index.js');
 const apiKey = process.env.API_KEY;
 
-// describe('function getLanguageList', () => {  
-//   it('should return an object with dirs key that contain a non empty array', () => {
-//     apiWrapper.getLanguageList(process.env.API_KEY).then((result) =>{
-//       expect(result).to.be.a('object');
-//       expect(result).to.include.all.keys('dirs')
-//       expect(result.dirs).to.be.an('array').that.is.not.empty;
-//     });
-//   });
-// });
-
 describe('function getLanguageList', () => {
   it('should return an object with dirs key that contain a non empty array', async () => {    
     const result = await apiWrapper.getLanguageList(process.env.API_KEY_YANDEX_TRANSLATE);
