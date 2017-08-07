5c769ad8c168efd93be3488196b799da9cc8c9e3
Tomas Ruiz <tomastrg22@gmail.com>
2017-08-07 21:44:59 +0200

    translateText test passed

--- test/test.js
+++ test/test.js
@@ -24,5 +24,14 @@ describe('function getLanguageList', () => {
     expect(result).to.include.all.keys('dirs')
     expect(result.dirs).to.be.an('array').that.is.not.empty;
   });
+});
 
-  
+describe('function translateText', () => {
+  it('should return text translated', async () => {
+    const result = await apiWrapper.translateText(process.env.API_KEY_YANDEX_TRANSLATE, 'es-en', 'Francia');
+    expect(result).to.be.a('object');
+    expect(result.code).to.equal(200)
+    expect(result.lang).to.equal('es-en');
+    expect(result.text[0]).to.equal('France')
+  });
+});
